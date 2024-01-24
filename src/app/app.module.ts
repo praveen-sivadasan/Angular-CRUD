b465bf13f567ca77140b66f8067a7a9bccc71753
Praveen Sivadasan <praveen.sivadasan@zoominfo.com>
2024-01-23 22:32:06 -0500

    feat: some new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserNameComponent } from './component/user-name/user-name.component';
+import { PrimaryButtonComponent } from './component/primary-button/primary-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserNameComponent,
+    PrimaryButtonComponent
   ],
   imports: [
     BrowserModule,
